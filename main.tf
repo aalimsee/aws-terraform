e02d1d87027afdbcd1de4fbac49e5c8f1d214c30
aaron lim <aaron.limse@hotmail.com>
2025-01-14 09:02:45 +0800

    edit source module path ./module

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "ec2_instance" {
-  source = file("./ec2_instance")
+  source = file("./module/ec2_instance")
   # why no depends_on?
 }
 
 module "elb" {
-  source = file("./elb")
+  source = file("./module/elb")
   depends_on = [module.ec2_instance]
 }
 
 module "eip" {
-  source = file("./eip")
+  source = file("./module/eip")
   depends_on = [module.elb]
 }
